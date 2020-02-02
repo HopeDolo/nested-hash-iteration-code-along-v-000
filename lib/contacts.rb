422df59fbf3f25159c7d3d01d3d0e9a2ad0706da
Darius Stewart <darius.stewart31@gmail.com>
2020-02-02 04:19:11 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,12 +14,17 @@ require 'pry'
   #   }
   # }
 
+#def remove_strawberry(contacts)
+#  contacts.each do |person, data|
+    #data.each do |attribute, value|
+      #if attribute == :favorite_ice_cream_flavors
+        #value.delete("strawberry")
+      #end
+    #end
+  #end
+#
 def remove_strawberry(contacts)
-  contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_ice_cream_flavors
-        value.delete("strawberry")
-      end
-    end
+  contacts.each do |person, contact_details_hash|
+    binding.pry 
   end
-end 
+  
